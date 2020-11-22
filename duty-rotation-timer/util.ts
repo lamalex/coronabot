4832a269f2971efcad2ad6f25c1a6e3b2c7e6eb1
Alexander Launi <alex.launi@gmail.com>
2020-11-22 09:15:10 -0500

    Remove everything like a freaking psycho

--- duty-rotation-timer/util.ts
+++
@@ -1,5 +0,0 @@
-export const zip = (a: any[], b: any[]): [any, any][] => {
-    return a.length >= b.length
-        ? a.map((item, i) => [item, b[i]])
-        : b.map((item, i) => [a[i], item]);
-};
